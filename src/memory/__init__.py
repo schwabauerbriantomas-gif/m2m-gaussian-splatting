672877625cd30aacb4069d67f2070bdf00bf26e5
schwabauerbriantomas-gif <schwabauerbriantomas@gmail.com>
2026-02-15 15:16:13 -0300

    Add files via upload

---
+++ src/memory/__init__.py
@@ -0,0 +1,11 @@
+"""
+Memory management module for M2M Gaussian Splatting.
+"""
+
+from .manager import SplatMemoryManager, MemoryConfig, MemoryStats
+
+__all__ = [
+    "SplatMemoryManager",
+    "MemoryConfig",
+    "MemoryStats",
+]
